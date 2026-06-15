456c4858d2a00e3c0c394f61984aa89d94a52d83
marienlund <hpj8260@yahoo.dk>
2026-06-15 14:31:14 +0200

    Add Sund Hund Yang - last avlshan for now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,6 @@ const maleDogs = mockDogs.filter((d) => d.gender === 'male')
 const extraFemales: { name: string }[] = []
 
 const extraMales = [
-  { name: "Sund Hund Yang" },
   { name: "Amager's Urban" },
   { name: "Jen-Ager's Odin" },
   { name: "Coudy z Udoli Upy" },
