4425585e73fe185e622be5074d8ed3bb557a1e17
marienlund <hpj8260@yahoo.dk>
2026-06-15 14:17:10 +0200

    Add Irck de la Hutte du Berger - Tysk mester 2017

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,6 @@ const maleDogs = mockDogs.filter((d) => d.gender === 'male')
 const extraFemales: { name: string }[] = []
 
 const extraMales = [
-  { name: "Irck de la Hutte du Berger" },
   { name: "Bayogi Eika" },
   { name: "Toftegården Nero" },
   { name: "Sund Hund Yang" },
