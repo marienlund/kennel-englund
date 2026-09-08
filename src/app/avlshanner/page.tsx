d95f41152b3596652d4b4f7b39a67cc150ddc79d
marienlund <marienlund@users.noreply.github.com>
2026-09-08 15:30:49 +0200

    Use uniform 4:3 photos on breeding dog pages

--- src/app/avlshanner/page.tsx
+++ src/app/avlshanner/page.tsx
@@ -44,7 +44,7 @@ export default async function AvlshannerPage() {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {males.map((dog) => (
-            <DogCard key={dog.id} dog={dog} />
+            <DogCard key={dog.id} dog={dog} uniformPhoto />
           ))}
         </div>
       )}
--- src/app/avlstaever/page.tsx
+++ src/app/avlstaever/page.tsx
@@ -47,7 +47,7 @@ export default async function AvlstaeverPage() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {females.map((dog) => (
-          <DogCard key={dog.id} dog={dog} />
+          <DogCard key={dog.id} dog={dog} uniformPhoto />
         ))}
       </div>
     </div>
